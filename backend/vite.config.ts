a4afcbdc0df43baadb1c0cdb22cfd0eab91aa600
Purvesh <ind.purvesh@gmail.com>
2023-06-26 08:42:44 +1200

    github pages

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: '../docs'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
