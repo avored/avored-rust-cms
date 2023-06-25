1b9216d7f8be0f713ccb4d9c6ea0179ff63cad37
Purvesh <ind.purvesh@gmail.com>
2023-06-26 08:50:26 +1200

    updated path

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -5,8 +5,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: ``,
   build: {
-    outDir: '../docs'
+    outDir: '../docs',
   },
   resolve: {
     alias: {
