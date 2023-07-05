95308b03667b16d9ccfb1579bb1a4a143d403bec
Purvesh <ind.purvesh@gmail.com>
2023-07-05 21:22:26 +1200

    role table in progres

---
+++ rust-api/migration/src/m20230705_082531_create_roles_table.rs
@@ -0,0 +1,49 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Roles::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Roles::Id)
+                            .uuid()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Roles::Name).string().not_null())
+                    .col(ColumnDef::new(Roles::Description).text().null())
+                    .col(ColumnDef::new(Roles::CreatedAt).timestamp().default(SimpleExpr::Keyword(Keyword::CurrentTimestamp)))
+                    .col(ColumnDef::new(Roles::UpdatedAt).timestamp().default(SimpleExpr::Keyword(Keyword::CurrentTimestamp)))
+                    .col(ColumnDef::new(Roles::CreatedBy).string().default(Value::String(Some(Box::new(String::from("CLI"))))))
+                    .col(ColumnDef::new(Roles::UpdatedBy).string().default(Value::String(Some(Box::new(String::from("CLI"))))))
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Roles::Table).to_owned())
+            .await
+    }
+}
+
+/// Learn more at https://docs.rs/sea-query#iden
+#[derive(Iden)]
+enum Roles {
+    Table,
+    Id,
+    Name,
+    Description,
+    CreatedAt,
+    UpdatedAt,
+    CreatedBy,
+    UpdatedBy
+}
