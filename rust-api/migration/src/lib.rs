eb642d67c6d58f7458e207f4146421270d76442c
Purvesh <ind.purvesh@gmail.com>
2023-07-05 21:42:58 +1200

    migration with admin users and roles

--- rust-api/migration/src/lib.rs
+++ rust-api/migration/src/lib.rs
@@ -2,6 +2,7 @@ pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_table;
 mod m20230705_082531_create_roles_table;
+mod m20230705_092720_create_admin_users_roles_table;
 
 pub struct Migrator;
 
@@ -11,6 +12,7 @@ impl MigratorTrait for Migrator {
         vec![
             Box::new(m20220101_000001_create_table::Migration),
             Box::new(m20230705_082531_create_roles_table::Migration),
+            Box::new(m20230705_092720_create_admin_users_roles_table::Migration),
         ]
     }
 }
--- rust-api/migration/src/m20220101_000001_create_table.rs
+++ rust-api/migration/src/m20220101_000001_create_table.rs
@@ -42,7 +42,7 @@ impl MigrationTrait for Migration {
 
 /// Learn more at https://docs.rs/sea-query#iden
 #[derive(Iden)]
-enum AdminUsers {
+pub enum AdminUsers {
     Table,
     Id,
     Name,
