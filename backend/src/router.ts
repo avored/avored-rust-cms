b7233551c1e71737beda11accbe5fcb9670d55b3
Purvesh <ind.purvesh@gmail.com>
2023-07-08 13:53:18 +1200

    npm run build

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from './views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
