f76a6d122f46612f05b3f889c81696bb7cfff5ca
Purvesh <ind.purvesh@gmail.com>
2023-07-18 08:16:01 +1200

    move the folder into root

--- backend/src/views/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="h-screen w-full flex items-center justify-center</div>">
-      <h2>About AvoRed Rust CMS</h2>
-    </div>
-  </template>
--- backend/src/vite-env.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="vite/client" />
-declare module '@vuelidate/core';
