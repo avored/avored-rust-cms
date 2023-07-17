3584219dfccadaed171c54302bd89982b32fc5b0
Purvesh <ind.purvesh@gmail.com>
2023-07-17 14:26:43 +1200

    layout setup

--- rust-api/src/handlers/get_admin_handler.rs
+++ rust-api/src/handlers/get_admin_handler.rs
@@ -3,22 +3,23 @@ use axum::{
     response::{Html, IntoResponse},
 };
 use axum_sessions::extractors::ReadableSession;
+use serde_json::json;
 use std::sync::Arc;
 
 use crate::{repositories::admin_user_repository::AdminUser, routes::AppState};
 
 pub async fn get_admin_handler(
-    _app_state: State<Arc<AppState>>,
+    app_state: State<Arc<AppState>>,
     session: ReadableSession,
 ) -> impl IntoResponse {
-    // let data = json!({});
+    let data = json!({});
 
-    // let handlebars = &app_state.handlebars;
+    let handlebars = &app_state.handlebars;
 
-    let session: AdminUser = session.get("logged_in_user").unwrap();
-    println!("Session: {:?}", session);
+    // let session: AdminUser = session.get("logged_in_user").unwrap();
+    // println!("Session: {:?}", session);
 
-    let html = "Admin PAge";
+    let html = handlebars.render("admin", &data).unwrap();
 
     Html(html).into_response()
 }
