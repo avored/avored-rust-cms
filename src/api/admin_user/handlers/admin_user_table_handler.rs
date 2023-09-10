543df66d74446ab5f019e1adcebb58f0009d4ef9
Subhajit Ghosh <99127578+subhajit20@users.noreply.github.com>
2023-09-10 11:58:20 +0000

    Added succes message to store_admin_user_handler

--- src/api/admin_user/handlers/admin_user_table_handler.rs
+++ src/api/admin_user/handlers/admin_user_table_handler.rs
@@ -14,7 +14,7 @@ use axum::{
 use serde::Serialize;
 
 pub async fn admin_user_table_handler(
-    session: AvoRedSession,
+    mut session: AvoRedSession,
     Query(query_param): Query<AdminUserTableQuery>,
     state: State<Arc<AvoRedState>>,
 ) -> Result<impl IntoResponse> {
@@ -29,9 +29,14 @@ pub async fn admin_user_table_handler(
         .admin_user_service
         .paginate(&state.db, current_page)
         .await?;
+    let success_message = session
+        .get("success_message")
+        .unwrap_or(String::from(""));
+        session.remove("success_message");
 
     let view_model = AdminUserTableViewModel {
         logged_in_user,
+        success_message,
         admin_user_pagination,
     };
 
