30f9a9936c49a32996a7ad07b56cba6f4b3d454d
Purvesh <ind.purvesh@gmail.com>
2023-09-16 11:30:56 +1200

    keep the upload and log folder

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -11,24 +11,18 @@ apt-get install -y \
   build-essential \
   openssl \
   libssl-dev \
-  pkg-config
-
-
-sudo apt install postgresql postgresql-contrib
+  pkg-config \
+  llvm-dev \
+  libclang-dev clang
 
 ## Install rustup and common components
 curl https://sh.rustup.rs -sSf | sh -s -- -y 
 rustup install nightly
 rustup component add rustfmt
-rustup component add rustfmt --toolchain nightly
 rustup component add clippy 
-rustup component add clippy --toolchain nightly
 
 cargo install cargo-expand
 cargo install cargo-edit
-
-
-cargo install sea-orm-cli
 
 
 ## setup and install oh-my-zsh
