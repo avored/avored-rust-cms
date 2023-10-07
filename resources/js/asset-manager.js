aa3f775f2fd7e53ec25521c1b063437cc778b5f6
Purvesh <ind.purvesh@gmail.com>
2023-10-07 15:58:15 +1300

    basic create/store asset route

--- resources/js/asset-manager.js
+++ resources/js/asset-manager.js
@@ -1,7 +1,26 @@
 export default () => ({
-
+    modal: false,
+    files: [],
     init() {
         console.log("asset manager")
     },
+    close_modal() {
+        this.modal = false
+    },
+    save_asset() {
+        console.log("Test Save Asset")
+        var formData = new FormData();
+        //var imagefile = document.querySelector('#file');
+        this.files.forEach((f) => formData.append('file', f));
+        //formData.append("image", imagefile.files[0]);
+        axios.post('/admin/store-asset', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+    },
+    file_on_change_alpine_js(e) {
+        this.files.push(e.target.files[0])
+    }
   });
   
