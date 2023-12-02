d2b5ba8dc454079b6b69735dad425dea0738c132
Purvesh <ind.purvesh@gmail.com>
2023-12-03 11:13:44 +1300

    react app setup

---
+++ react-admin/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="h-screen flex w-full justify-center items-center">
+        <h1 className="text-3xl font-bold text-indigo-600">
+          AvoRed rust content management system
+        </h1>
+      </div>
+  );
+}
+
+export default App;
