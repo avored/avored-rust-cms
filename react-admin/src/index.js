2eba7b050a890c5eedc3166c4e8d46cd097965d9
syazell1 <mafty.erin01@gmail.com>
2024-04-06 13:58:19 +0800

    add query client to the root

--- react-admin/src/index.js
+++ react-admin/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {QueryClientProvider, QueryClient} from '@tanstack/react-query'
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
+  </QueryClientProvider>
   // </React.StrictMode>
 );
 
