1b94380da5ff2a228908a4cc6ed78492391cd85b
syazell1 <mafty.erin01@gmail.com>
2024-04-06 13:59:21 +0800

    add reusable hook for login

---
+++ react-admin/src/pages/auth/hooks/useLogin.js
@@ -0,0 +1,21 @@
+import {useMutation} from '@tanstack/react-query'
+import { useAxios } from '../../../hooks/useAxios';
+import {useNavigate} from 'react-router-dom'
+
+export const useLogin = () => {
+    const client = useAxios();
+    const redirect = useNavigate();
+    return useMutation({
+        mutationFn: async (data) => {
+            const res = await client.post('/login', data);
+
+            return res;
+        },
+        onSuccess: (res) => {
+            // will set the res.data.data (which is the access token)
+            localStorage.setItem("AUTH_TOKEN", res.data.data);
+            // and redirect to the admin.
+            redirect("/admin");
+        }
+   }) 
+}
