d72867d457e44746322327bcccff91de5504520e
Purvesh <ind.purvesh@gmail.com>
2024-06-23 13:04:48 +1200

    support for sorting

--- react-admin/src/pages/admin-user/hooks/useAdminUserTable.ts
+++ react-admin/src/pages/admin-user/hooks/useAdminUserTable.ts
@@ -1,22 +1,33 @@
-import {useQuery} from '@tanstack/react-query'
+import {useQuery, useQueryClient} from '@tanstack/react-query'
 import { useAxios } from '../../../hooks/useAxios'
 import _ from 'lodash'
 import {useNavigate} from 'react-router-dom'
 
-export const useAdminUserTable = () => {
+type PaginateType = {
+    order?: string;
+}
+
+export const useAdminUserTable = (query: PaginateType) => {
+    let query_string = "";
+    if (query.order !== "") {
+        const queryKey = query.order
+        query_string = "?" + new URLSearchParams(query).toString()
+    }
+
     const client = useAxios();
     const redirect = useNavigate();
     return useQuery({
         queryKey: ['admin-user-table'],
         queryFn: (async () => {
             try {
-                return await client.get("/admin-user")
+                return await client.get("/admin-user" + query_string)
             } catch (error) {
                 if (_.get(error, 'response.status') === 401) {
                     localStorage.removeItem('AUTH_TOKEN')
                     redirect("/admin/login")
                 }
             }
-        })
+        }),
+
     })
 }
