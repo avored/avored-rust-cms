6bd177cacc0fe565b208cd74e3e662afb9828e9d
Purvesh <ind.purvesh@gmail.com>
2024-06-25 21:32:10 +1200

    git action

--- react-admin/vite.config.ts
+++ react-admin/vite.config.ts
@@ -4,8 +4,6 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
 import svgr from "vite-plugin-svgr"
 
 export default defineConfig({
-    // depending on your application, base can also be "/"
-    base: '',
     plugins: [react(), viteTsconfigPaths(), svgr()],
     server: {
         // this ensures that the browser opens upon server start
