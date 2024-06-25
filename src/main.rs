ee20b249e6a31c42f2f599777f397e7d48573a66
Purvesh <ind.purvesh@gmail.com>
2024-06-25 22:20:51 +1200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -48,11 +48,11 @@ async fn main() -> Result<()> {
 
     println!();
     println!();
-    println!("Server started: http://0.0.0.0:8080");
+    println!("Server started: http://0.0.0.0:8081");
 
     // region:    --- Start Server
     // let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8081").await.unwrap();
     info!("{:<12} - on {:?}\n", "LISTENING", listener.local_addr());
     axum::serve(listener , app.into_make_service())
         .await
