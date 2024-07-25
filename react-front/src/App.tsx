17ae03877dfe07fc97417de70cc98ddfecff0ab5
Purvesh <ind.purvesh@gmail.com>
2024-07-25 21:23:32 +1200

    react front in progress

--- react-front/src/App.tsx
+++ react-front/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from './assets/logo_only.svg'
 import './App.css'
 
 function App() {
@@ -9,25 +8,16 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://avored.com" target="_blank">
+          <img src={logo} className="logo" alt="AvoRed logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>AvoRed content management system</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
