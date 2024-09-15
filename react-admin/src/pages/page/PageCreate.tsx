41b4d560413ef9b7e9351fea5002bb17fae0feed
Purvesh <ind.purvesh@gmail.com>
2024-09-15 13:24:19 +1200

    update page ui improvements

--- react-admin/src/pages/page/PageCreate.tsx
+++ react-admin/src/pages/page/PageCreate.tsx
@@ -38,159 +38,7 @@ function PageCreate() {
     name: "page_fields", //rename fields
   });
 
-  // const setSelectedElementDataOption = (
-  //   selected: Array<string>,
-  //   pageComponentIndex: number,
-  //   componentElementIndex: number,
-  // ) => {
-  //   let val = selected.pop() ?? "";
-  //
-  //   // @todo fix this onese
-  //   // Currently all data types is text but will be depend on element_type???
-  //   setValue(
-  //     `components_content.${pageComponentIndex}.elements.${componentElementIndex}.element_data_type`,
-  //     "TEXT",
-  //   );
-  //   setValue(
-  //     `components_content.${pageComponentIndex}.elements.${componentElementIndex}.element_content`,
-  //     val,
-  //   );
-  // };
-
-  // const getSelectElementDataOptionCurrentValue = (
-  //   pageComponentIndex: number,
-  //   componentElementIndex: number,
-  // ) => {
-  //   let val = getValues(
-  //     `components_content.${pageComponentIndex}.elements.${componentElementIndex}.element_content`,
-  //   );
-  //   if (val) {
-  //     return [val];
-  //   }
-  //   return [];
-  // };
-
-  const component_all_api_response = useComponentAll();
-  // const components = _.get(component_all_api_response, "data.data", []);
   const { mutate, error } = useStorePage();
-
-  // const renderComponentElementType = (
-  //   componentElement: ICreatablePageComponentElementModel,
-  //   pageComponentIndex: number,
-  //   componentElementIndex: number,
-  // ) => {
-  //   switch (componentElement.element_type) {
-  //     case "textarea":
-  //       return (
-  //         <div className="p-3">
-  //           <textarea
-  //             {...register(
-  //               `components_content.${pageComponentIndex}.elements.${componentElementIndex}.element_content`,
-  //             )}
-  //           ></textarea>
-  //           <label
-  //             htmlFor={componentElement.identifier}
-  //             className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-  //           >
-  //             {componentElement.name}
-  //           </label>
-  //         </div>
-  //       );
-  //     case "select":
-  //       return (
-  //         <div className="p-3">
-  //           <AvoRedMultiSelectField
-  //             label="test dropdown"
-  //             selectedOption={getSelectElementDataOptionCurrentValue(
-  //               pageComponentIndex,
-  //               componentElementIndex,
-  //             )}
-  //             options={componentElement.element_data ?? []}
-  //             onChangeSelectedOption={(val: Array<string>) =>
-  //               setSelectedElementDataOption(
-  //                 val,
-  //                 pageComponentIndex,
-  //                 componentElementIndex,
-  //               )
-  //             }
-  //           ></AvoRedMultiSelectField>
-  //           <label
-  //             htmlFor={componentElement.identifier}
-  //             className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-  //           >
-  //             {componentElement.name}
-  //           </label>
-  //         </div>
-  //       );
-  //     case "radio":
-  //       return (
-  //         <div className="p-3">
-  //           <input
-  //             id={componentElement.identifier}
-  //             type="radio"
-  //             value={componentElement.identifier}
-  //             className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-  //           />
-  //           <label
-  //             htmlFor={componentElement.identifier}
-  //             className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-  //           >
-  //             {componentElement.name}
-  //           </label>
-  //         </div>
-  //       );
-  //     case "text":
-  //     default:
-  //       return (
-  //         <div>
-  //           <InputField
-  //             name={`components_content.${pageComponentIndex}.elements.${componentElementIndex}.element_content`}
-  //             label={componentElement.name}
-  //             register={register(
-  //               `components_content.${pageComponentIndex}.elements.${componentElementIndex}.element_content`,
-  //             )}
-  //           />
-  //           <InputField
-  //             type="hidden"
-  //             value="TEXT"
-  //             register={register(
-  //               `components_content.${pageComponentIndex}.elements.${componentElementIndex}.element_data_type`,
-  //             )}
-  //           />
-  //         </div>
-  //       );
-  //   }
-  // };
-
-  // const renderComponentElement = (
-  //   componentElement: ICreatablePageComponentElementModel,
-  //   pageComponentIndex: number,
-  //   componentElementIndex: number,
-  // ) => {
-  //   return (
-  //     <div
-  //       className="ring-1 my-2 ring-gray-200"
-  //       key={componentElement.identifier}
-  //     >
-  //       {JSON.stringify(componentElement)}
-  //       {renderComponentElementType(
-  //         componentElement,
-  //         pageComponentIndex,
-  //         componentElementIndex,
-  //       )}
-  //     </div>
-  //   );
-  // };
-
-  // const componentSelected = (e: React.MouseEvent, componentId: string) => {
-  //   e.preventDefault();
-  //   const selectedComponent = components.find(
-  //     (component: IComponentModel) => component.id === componentId,
-  //   );
-  //   setIsComponentTableModalOpen(false);
-  //
-  //   append(selectedComponent);
-  // };
 
   const addPageFieldOnClick = (e: any) => {
     e.preventDefault();
@@ -207,39 +55,7 @@ function PageCreate() {
     remove(index);
   };
 
-  // const pageModelOnClose = () => {
-  //   setIsComponentTableModalOpen(false);
-  // };
-
-  // const renderComponent = (
-  //   pageComponent: ICreatablePageComponentModel,
-  //   pageComponentIndex: number,
-  // ) => {
-  //   return (
-  //     <div
-  //       key={pageComponent.id}
-  //       className="my-5 ring-1 ring-gray-200 rounded p-3"
-  //     >
-  //       <div>component name: {pageComponent.name}</div>
-  //       <div>component identifier: {pageComponent.identifier}</div>
-  //       <div>
-  //         Component Fields
-  //         {pageComponent.elements.map(
-  //           (componentElement, componentElementIndex) => {
-  //             return renderComponentElement(
-  //               componentElement,
-  //               pageComponentIndex,
-  //               componentElementIndex,
-  //             );
-  //           },
-  //         )}
-  //       </div>
-  //     </div>
-  //   );
-  // };
-
   const submitHandler = async (data: CreatablePageType) => {
-    // console.log(data)
     mutate(data);
   };
 
@@ -405,14 +221,6 @@ function PageCreate() {
                 );
               })}
 
-              {/*<div>*/}
-              {/*  {components_content.map(*/}
-              {/*    (pageComponent, pageComponentIndex) => {*/}
-              {/*      return renderComponent(pageComponent, pageComponentIndex);*/}
-              {/*    },*/}
-              {/*  )}*/}
-              {/*</div>*/}
-
               <div className="mb-4 flex items-center justify-center ring-1 ring-gray-400 rounded p-5">
                 <button
                   type="button"
@@ -426,16 +234,6 @@ function PageCreate() {
                 </button>
               </div>
 
-              {/*<AvoredModal*/}
-              {/*    closeModal={pageModelOnClose}*/}
-              {/*    modal_header={t("select_element")}*/}
-              {/*    modal_body={*/}
-              {/*        <div className="text-primary-500">*/}
-              {/*            Test*/}
-              {/*        </div>*/}
-              {/*    }*/}
-              {/*    isOpen={isComponentTableModalOpen}*/}
-              {/*></AvoredModal>*/}
               <hr />
               <div className="mt-5  flex items-center">
                 <button
@@ -445,7 +243,7 @@ function PageCreate() {
                   {t("save")}
                 </button>
                 <Link
-                  to="/admin/page"
+                  to={`/admin/page`}
                   className="ml-auto font-medium text-gray-600 hover:text-gray-500"
                 >
                   {t("cancel")}
