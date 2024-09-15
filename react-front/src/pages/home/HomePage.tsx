bc90e3e60eb92c024d09bbd84a41fff5f6e67678
Purvesh <ind.purvesh@gmail.com>
2024-09-15 15:32:23 +1200

    fixed the demo data

--- react-front/src/pages/home/HomePage.tsx
+++ react-front/src/pages/home/HomePage.tsx
@@ -4,21 +4,17 @@ import MainHeroSection from "./MainHeroSection";
 import RepositoryInformation from "./RepositoryInformation";
 import {useHomeCmsPage} from "./hooks/useHomeCmsPage";
 import _ from "lodash";
-import {ComponentContentType} from "../../types/CmsPageType";
+import {PageFieldType} from "../../types/CmsPageType";
 
 const HomePage = () => {
-    const {data: home_page_model} = useHomeCmsPage()
-    const GetComponent = ((component_identifier: string): ComponentContentType | undefined => {
-        const components =  _.get(home_page_model, 'data.data.page_model.components_content', [])
-        return components.find((com: ComponentContentType) => {
-            return com.identifier === component_identifier
-        })
+    const {data} = useHomeCmsPage()
+    const GetPageFields = ((): Array<PageFieldType> => {
+        return _.get(data, 'data.data.page_model.page_fields', [])
     })
-    // console.log(home_page_model?.data.data?.page_model?.components_content)
   return (
     <>
       <div className="relative bg-white overflow-hidden">
-        <MainHeroSection HeroComponent={GetComponent('hero-component')} />
+        <MainHeroSection page_fields={GetPageFields()} />
 
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
           <img
@@ -29,9 +25,9 @@ const HomePage = () => {
         </div>
       </div>
 
-      <RepositoryInformation RepositoryComponent={GetComponent('repository-component')} />
-      <FeaturesSection KeyFeaturesComponent={GetComponent('key-features-component')} />
-      <ContactSection ContactUsComponent={GetComponent('contact-us-component')} />
+      <RepositoryInformation RepositoryComponent={GetPageFields()} />
+      <FeaturesSection KeyFeaturesComponent={GetPageFields()} />
+      <ContactSection ContactUsComponent={GetPageFields()} />
     </>
   );
 };
--- react-front/src/pages/home/MainHeroSection.tsx
+++ react-front/src/pages/home/MainHeroSection.tsx
@@ -1,12 +1,12 @@
-import _ from "lodash";
-import {ComponentContentType} from "../../types/CmsPageType";
+import {PageFieldType} from "../../types/CmsPageType";
 import {GetElementValue} from "../../lib/page";
 
-type HeroComponentProps = {
-    HeroComponent?: ComponentContentType
+type PageFieldsProps = {
+    page_fields: PageFieldType[]
 }
 
-const MainHeroSection = (({HeroComponent}: HeroComponentProps) => {
+const MainHeroSection = ((props: PageFieldsProps) => {
+    console.log("props hero ", props)
     return (
         <>
         <div className="mx-auto">
@@ -14,29 +14,29 @@ const MainHeroSection = (({HeroComponent}: HeroComponentProps) => {
             <div className="mt-10 mx-auto px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <div className="sm:text-center lg:text-left">
                 <h2 className="text-3xl tracking-tight leading-10 heading-font font-extrabold text-gray-900 sm:text-4xl sm:leading-none md:text-5xl">
-                    {GetElementValue(HeroComponent, 'hero-page-title')}
+                    {GetElementValue(props.page_fields, 'hero-page-title')}
                 </h2>
 
                 <p className="mt-3 text-gray-500 sm:mt-5  sm:max-w-xl sm:mx-auto md:mt-5 lg:mx-0">
-                    {GetElementValue(HeroComponent, 'hero-page-description')}
+                    {GetElementValue(props.page_fields, 'hero-page-description')}
                 </p>
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                   <div className="rounded-md shadow">
                     <a
-                      href={GetElementValue(HeroComponent, 'get-started-button-url')}
+                      href={GetElementValue(props.page_fields, 'get-started-button-url')}
                       target="_blank"
                       className="w-full flex items-center justify-center bg-gradient-to-b from-primary-700 via-primary-600 to-primary-700 hover:bg-gradient-to-t hover:from-primary-800 hover:via-primary-700 hober:to-primary-700 px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-primary-600 hover:bg-primary-500 hover:text-white focus:outline-none focus:shadow-outline transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
                     >
-                        {GetElementValue(HeroComponent, 'get-started-button-title')}
+                        {GetElementValue(props.page_fields, 'get-started-button-title')}
                     </a>
                   </div>
                   <div className="mt-3 sm:mt-0 sm:ml-3">
                     <a
-                      href={`${GetElementValue(HeroComponent, "demo-button-url")}`}
+                      href={`${GetElementValue(props.page_fields, "demo-button-url")}`}
                       target="_blank"
                       className="w-full flex items-center justify-center px-8 py-3 border bg-gradient-to-b from-gray-400 via-gray-300 to-gray-400 hover:bg-gradient-to-t hover:from-gray-300 hover:via-gray-400 hober:to-gray-300 border-transparent text-base leading-6 font-medium rounded-md text-white-700 bg-primary-100 hover:text-gray-700 hover:bg-primary-50 focus:outline-none focus:shadow-outline focus:border-primary-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
                     >
-                        {GetElementValue(HeroComponent, 'demo-button-title')}
+                        {GetElementValue(props.page_fields, 'demo-button-title')}
                     </a>
                   </div>
                 </div>
