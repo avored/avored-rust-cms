405862fb105270ca0d1a637b42776f6f4e9a5e21
Purvesh <ind.purvesh@gmail.com>
2025-07-05 11:16:51 +1200

    node js

--- ts-grpc-react-admin/src/hooks/admin_user/UseDeleteAdminUserHook.ts
+++ ts-grpc-react-admin/src/hooks/admin_user/UseDeleteAdminUserHook.ts
@@ -1,7 +1,7 @@
 import {useMutation} from "@tanstack/react-query";
 import {useNavigate} from "react-router-dom";
 import {AdminUserClient} from "../../grpc_generated/Admin_userServiceClientPb";
-import {DeleteAdminUserRequest, DeleteRoleRequest} from "../../grpc_generated/admin_user_pb";
+import {DeleteAdminUserRequest} from "../../grpc_generated/admin_user_pb";
 
 export const UseDeleteAdminUserHook = () => {
     const backend_url: string = process.env.REACT_APP_BACKEND_BASE_URL ?? "http://localhost:50051";
