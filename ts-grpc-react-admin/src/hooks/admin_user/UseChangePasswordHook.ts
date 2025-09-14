25a6bbdb409a18a6e9d7f0c2e8813a8be970ceb6
Purvesh <ind.purvesh@gmail.com>
2025-09-14 16:06:22 +1200

    Update UseChangePasswordHook.ts

--- ts-grpc-react-admin/src/hooks/admin_user/UseChangePasswordHook.ts
+++ ts-grpc-react-admin/src/hooks/admin_user/UseChangePasswordHook.ts
@@ -1,12 +1,12 @@
 import {useMutation} from "@tanstack/react-query";
-import {useNavigate} from "react-router-dom";
+// import {useNavigate} from "react-router-dom";
 import {AdminUserClient} from "../../grpc_generated/Admin_userServiceClientPb";
 import { GetAdminUserRequest} from "../../grpc_generated/admin_user_pb";
 
 export const UseChangePasswordHook = () => {
     const backend_url: string = process.env.REACT_APP_BACKEND_BASE_URL ?? "http://localhost:50051";
     const client = new AdminUserClient(backend_url);
-    const redirect = useNavigate();
+    // const redirect = useNavigate();
 
     return useMutation({
         mutationFn: (request: any) => {
