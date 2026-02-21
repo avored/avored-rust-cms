d9b8aa4a7f2c4495acfd1a8bac0473e183c6b9a8
Purvesh <ind.purvesh@gmail.com>
2026-02-22 10:15:09 +1300

    log setup

--- src/infra/setup.rs
+++ src/infra/setup.rs
@@ -1,8 +1,19 @@
 use axum::extract::FromRef;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{filter, Layer};
+use std::env;
+use std::fs::File;
+use std::path::Path;
+use std::sync::Arc;
 use leptos::config::{LeptosOptions, get_configuration};
 use crate::{error::Result};
 
 pub async fn init_app_state() -> Result<AppState> {
+
+    init_log();
+    // rust_i18n::i18n!("resources/locales");
+
     let conf = get_configuration(None)?;
     // let addr = conf.leptos_options.site_addr;
 
@@ -23,4 +34,46 @@ impl FromRef<AppState> for LeptosOptions {
     fn from_ref(app_state: &AppState) -> Self {
         app_state.leptos_options.clone()
     }
+}
+
+
+
+fn init_log() {
+    let stdout_log = tracing_subscriber::fmt::layer().pretty();
+
+    // A layer that logs events to a file.
+    let log_dir = env::var("AVORED_LOG_DIR").unwrap_or_else(|_| "public/log".to_string());
+    let log_file = env::var("AVORED_LOG_FILE").unwrap_or_else(|_| "avored.log".to_string());
+    let file = File::create(Path::new(&log_dir).join(&log_file));
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => panic!("Error: {error:?}"),
+    };
+    let debug_log = tracing_subscriber::fmt::layer().with_writer(Arc::new(file));
+
+    // A layer that collects metrics using specific events.
+    let metrics_layer = /* ... */ filter::LevelFilter::INFO;
+
+    tracing_subscriber::registry()
+        .with(
+            stdout_log
+                // Add an `INFO` filter to the stdout logging layer
+                .with_filter(filter::LevelFilter::INFO)
+                // Combine the filtered `stdout_log` layer with the
+                // `debug_log` layer, producing a new `Layered` layer.
+                .and_then(debug_log)
+                // Add a filter to *both* layers that rejects spans and
+                // events whose targets start with `metrics`.
+                .with_filter(filter::filter_fn(|metadata| {
+                    !metadata.target().starts_with("metrics")
+                })),
+        )
+        .with(
+            // Add a filter to the metrics label that *only* enables
+            // events whose targets start with `metrics`.
+            metrics_layer.with_filter(filter::filter_fn(|metadata| {
+                metadata.target().starts_with("metrics")
+            })),
+        )
+        .init();
 }
